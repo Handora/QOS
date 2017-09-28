c4b1cdcf28d1afdf601379baae618df99ff8d818
Handora <qcdsr970209@gmail.com>
2017-09-29 00:05:07 +0800

    LAB4 PART B START

--- kern/syscall.c
+++ kern/syscall.c
@@ -144,7 +144,14 @@ static int
 sys_env_set_pgfault_upcall(envid_t envid, void *func)
 {
 	// LAB 4: Your code here.
-	panic("sys_env_set_pgfault_upcall not implemented");
+    int err;
+    struct Env *e;
+    if ((err=envid2env(envid, &e, 1)) < 0) {
+        return err;
+    }
+
+    e->env_pgfault_upcall = func;
+    return 0;
 }
 
 // Allocate a page of memory and map it at 'va' with permission
@@ -393,6 +400,8 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
         return sys_page_unmap((envid_t)a1, (void *)a2);
     case SYS_env_set_status:
         return sys_env_set_status((envid_t)a1, (int)a2);
+    case SYS_env_set_pgfault_upcall:
+        return sys_env_set_pgfault_upcall((envid_t)a1, (void *)a2);
 	default:
 		return -E_INVAL;
 	}
