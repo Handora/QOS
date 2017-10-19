fa655d5160bed955290238c259117f6e6a4f8138
Handora <qcdsr970209@gmail.com>
2017-10-19 13:24:46 +0800

    lab4 sfork doing

---
+++ user/sfork.c
@@ -0,0 +1,22 @@
+#include <inc/lib.h>
+
+uint32_t share_val = 0;
+
+void umain(int argc, char* argv[]) {
+        envid_t who;
+        if((who = sfork()) == 0) {
+            cprintf("child: %d\n", share_val);
+            share_val = 1000;
+            sys_yield();
+            cprintf("child: %d\n", share_val);
+            share_val = 10000;
+            return;
+        }
+
+        sys_yield();
+        cprintf("parent: %d\n", share_val);
+        share_val++;
+        sys_yield();
+        cprintf("parent: %d\n", share_val);
+        return;
+}
